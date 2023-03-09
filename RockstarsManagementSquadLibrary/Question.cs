37f0e08e38794e1874d2f4e11ffa58d0ad4cd032
Bas Bakens <bas.bakens@live.nl>
2023-03-09 11:08:01 +0100

    added constructor to answer and question class

--- RockstarsManagementSquadLibrary/Question.cs
+++ RockstarsManagementSquadLibrary/Question.cs
@@ -7,8 +7,20 @@ namespace RockstarsManagementSquadLibrary
 {
     public class Question
     {
+        // properties
         public int Id { get; private set; }
         public string Question_ { get; private set; }
         public string Description { get; private set; }
+
+        // constructors
+        public Question(int id, string question, string description)
+        {
+            Id = id;
+            Question_ = question;
+            Description = description;
+        }
+
+        // methods
+
     }
 }
