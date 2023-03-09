49633ad91d4af3a69f12c23f949e3ad50e41cf22
Kian Bruyenberg <kbruyenberg@gmail.com>
2023-03-09 14:07:49 +0100

    Frontend navbar added, deleted home controller
    Added a navbar and deleted the home controller

--- RockstarsManagementSquad/Controllers/DashboardController.cs
+++ RockstarsManagementSquad/Controllers/DashboardController.cs
@@ -1,12 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
+using RockstarsManagementSquad.Models;
+using System.Diagnostics;
 
-namespace RockstarsManagementSquad.Controllers;
-
-public class DashboardController : Controller
+namespace RockstarsManagementSquad.Controllers
 {
-    // GET
-    // public IActionResult Index()
-    // {
-    //     return View();
-    // }
+    public class DashboardController : Controller
+    {
+        private readonly ILogger<DashboardController> _logger;
+
+        public DashboardController(ILogger<DashboardController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public UserViewModel CreateUser(string name, string email, string emailConfirmed, string password, string passwordConfirmed)
+        {
+            UserViewModel user = new UserViewModel(name, email.ToLower(), emailConfirmed.ToLower(), password, passwordConfirmed);
+
+            return user;
+        }
+    }
 }
