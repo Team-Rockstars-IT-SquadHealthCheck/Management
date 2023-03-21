64ed3211df84ddff63a6fbd0c40da7a1065a147f
Kian Bruyenberg <kbruyenberg@gmail.com>
2023-03-21 11:49:10 +0100

    Added enquete functionality

--- RockstarsManagementSquad/Controllers/EnquêtesController.cs
+++ RockstarsManagementSquad/Controllers/EnquêtesController.cs
@@ -1,11 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using RockstarsManagementSquad.Models;
+using RockstarsManagementSquad.Models.DTO;
+using RockstarsManagementSquad.Services.Interfaces;
+using RockstarsManagementSquadLibrary;
 
 namespace RockstarsManagementSquad.Controllers;
 
 public class EnquêtesController : Controller
 {
-     public IActionResult Index()
+    private readonly Services.Interfaces.ICompanyViewModelService _service;
+
+    public CompaniesController(RockstarsManagementSquad.Services.Interfaces.ICompanyViewModelService service)
     {
-        return View();
+        _service = service ?? throw new ArgumentNullException(nameof(service));
+    }
+    public IActionResult Index()
+    {
+        //var companies = await _service.Find();
+        var products = await _service.Find();
+        // foreach company create new in view model
+        foreach (var product in products)
+        {
+            CompanyViewModel companyViewModel = new CompanyViewModel();
+        }
+        return View(products);
     }
 }
