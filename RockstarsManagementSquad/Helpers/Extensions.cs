f931879ef7825d2ee9c615d92e22e8924e40919a
Bas Bakens <bas.bakens@live.nl>
2023-06-08 11:36:01 +0200

    Fixed variable questions (survey can not be created because surveyViewModel object isn't given back to controller)

--- RockstarsManagementSquad/Helpers/Extensions.cs
+++ RockstarsManagementSquad/Helpers/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using RockstarsManagementSquad.Models;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using RockstarsManagementSquad.Models;
 using RockstarsManagementSquad.Models.DTO;
 using RockstarsManagementSquadLibrary;
 
@@ -14,6 +15,10 @@ namespace RockstarsManagementSquad.Helpers
         {
             return new SurveyDTO(survey.Id, survey.Name, survey.Description);
         }
+        public static SurveyViewModel ConvertSurveyToSurveyViewModel(this Survey survey)
+        {
+            return new SurveyViewModel(survey.Id, survey.Name, survey.Description);
+        }
         public static Survey ConvertSurveyDTOToSurvey(this SurveyDTO sDTO)
         {
             return new Survey(sDTO.id, sDTO.name, sDTO.description);
@@ -22,9 +27,25 @@ namespace RockstarsManagementSquad.Helpers
         {
             return new SurveyViewModel(sDTO.id, sDTO.name, sDTO.description);
         }
+        public static SurveyDTO ConvertSurveyViewModelToSurveyDTO(this SurveyViewModel svm)
+        {
+            return new SurveyDTO(svm.id, svm.name, svm.description);
+        }
         public static QuestionDTO ConvertQuestionToQuestionDTO(this Question question)
         {
             return new QuestionDTO(question._Question, question.Description, question.SurveyId, question.Desc_good, question.Desc_avg, question.Desc_bad);
+        }
+        public static Question ConvertQuestionViewModelToQuestion(this QuestionViewModel qvm)
+        {
+            return new Question(qvm.question, qvm.description, qvm.surveyid, qvm.desc_good, qvm.desc_avg, qvm.desc_bad);
+        }
+        public static QuestionDTO ConvertQuestionViewModelToQuestionDTO(this QuestionViewModel qvm)
+        {
+            return new QuestionDTO(qvm.question, qvm.description, qvm.surveyid, qvm.desc_good, qvm.desc_avg, qvm.desc_bad);
+        }
+        public static QuestionViewModel ConvertQuestionToQuestionViewModel(this Question question)
+        {
+            return new QuestionViewModel(question._Question, question.Description, question.SurveyId, question.Desc_good, question.Desc_avg, question.Desc_bad);
         }
     }
 }
